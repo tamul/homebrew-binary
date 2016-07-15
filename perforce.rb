b3962c6fa87fed49e824fbd497bb6a9a8a641da1
Tayler Mulligan <mulligantayler@gmail.com>
2016-07-14 23:37:30 -0400

    Update SHA256 sums for new version
    File pointed to by URL was updated to new version, fix SHA256 sum (correctly) mismatching

--- perforce.rb
+++ perforce.rb
@@ -1,14 +1,14 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2016.1.1404799"
+  version "2016.1.1411799"
 
   if MacOS.prefer_64_bit?
-    url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86_64/p4"
-    sha256 "0f91b86f11e98ac8078500da392d4a55a65d74899bd507133c40de3cb220feb4"
+    url "http://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86_64/p4"
+    sha256 "85153a60d75d84b172320e0d9348d0da67979f24a2e63650e5febf005afac778"
   else
-    url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86/p4"
-    sha256 "4f5c9ad5f46497f16afc06a7c7e755079d754dfd64c3eaf229e3e51643db6e7d"
+    url "http://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86/p4"
+    sha256 "ab769f704c20a5521b0994732af60187c7a99b1442e6d3a019fda63de748c696"
   end
 
   bottle :unneeded
